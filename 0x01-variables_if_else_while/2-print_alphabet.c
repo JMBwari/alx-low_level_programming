d5cb96dbf2587cb35b3916d7ebe77985853619fa
JMBwari <ondieki.marion@gmail.com>
2022-09-09 12:22:43 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if successful.
+*/
+int main(void)
+{
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	
+	putchar(13);
+
+	return (0);
+}
+
