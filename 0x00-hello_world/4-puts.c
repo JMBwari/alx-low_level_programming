021e8844628e0f28603058adc2f25989feaef824
JMBwari <ondieki.marion@gmail.com>
2022-09-09 03:53:20 -0700

    fifth task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *
+ *  A simple program using puts.
+ *
+ *   */
+
+int main(void)
+
+{
+
+	        puts("\"Programming is like building a multilingual puzzle");
+
+		        return 0;
+
+}
