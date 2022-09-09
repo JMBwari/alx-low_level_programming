61d7a21f3dd55433f266b015bd7ac0c3fe0e9e57
JMBwari <ondieki.marion@gmail.com>
2022-09-09 12:53:29 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i = 123; i++)
+	{
+		putchar(i);
+	}
+	else for (i = 65; i = 91; i++)
+	{
+		putchar(i);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
