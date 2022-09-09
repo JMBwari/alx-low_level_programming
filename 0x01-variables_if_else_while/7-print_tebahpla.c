db14f148770be343ffa021c553dbdcf3e460221e
JMBwari <ondieki.marion@gmail.com>
2022-09-09 14:48:46 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	int i;
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+}
