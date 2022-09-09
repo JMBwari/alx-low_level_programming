d141a3ddfd234716a872bc1dd13cf5fce203fb5d
JMBwari <ondieki.marion@gmail.com>
2022-09-09 08:05:40 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Main - Prints out the last part of a quote in the stderr.
- * Return: 1 if success.
+ * main - Prints out the last part of a quote in the stderr.
+ * return: 1 if success.
  */
 int main(void)
 {
