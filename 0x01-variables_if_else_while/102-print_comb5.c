c8133adf7021a505a9c667901964d04a4465aa38
JMBwari <ondieki.marion@gmail.com>
2022-09-11 07:01:54 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					if ((i == j) || (i == k) || (i == l) || (j == k) || (j == l))
+					{
+						continue;
+					}
+
+					if ((i > j) || (j > k) || (k > l))
+					{
+						continue;
+					}
+
+					if ((!(i == 48) && (j == 49) && (k == 50) && (l == 51)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
