d25c5ebff4c7873484a391d31c21a9a80d15da8e
JMBwari <ondieki.marion@gmail.com>
2022-09-13 10:41:04 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int h;
+	int i;
+	
+	for (h = 1; h < 11; h++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+}
