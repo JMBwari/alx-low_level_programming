6f14d920e41b1e2b5a63d44235907cfc50b89dda
JMBwari <ondieki.marion@gmail.com>
2022-09-13 14:01:12 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-unsigned int absu(int value);
-
 /**
  * print_last_digit - a function that prints the last digit of a number.
  * Return: value of the last digit
@@ -10,17 +8,15 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
-	n = absu(n);
-
 	/** Find last digit of a number */
 	lastDigit = n % 10;
+
+	if (lastDigit < 0)
+	{
+		lastDigit = lastDigit * -1;
+	}
 
 	_putchar('0' + lastDigit);
 
 	return (lastDigit);
-}
-
-unsigned int absu(int value)
-{
-	return (value < 0) ? -((unsigned int)value) : (unsigned int)value;
 }
