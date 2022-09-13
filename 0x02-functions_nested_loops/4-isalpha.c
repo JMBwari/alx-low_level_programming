571e9bd2195732b0b7fe7a55e4975cb729ca50b3
JMBwari <ondieki.marion@gmail.com>
2022-09-13 11:40:56 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,7 @@ int _isalpha(int c)
 {
 	/** checks if c falls in the range of 97 to 122
 	 * or if falls in the range of 65 to 90 */
-	if ((c >= 97) && c <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
