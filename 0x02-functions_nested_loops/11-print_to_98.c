5cee2d17cce3a4b585b212d171040c01f48f1daa
JMBwari <ondieki.marion@gmail.com>
2022-09-14 07:08:48 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,19 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 99; i++)
+	if (n <= 98)
 	{
-		printf("%d", i);
+		for (i = n; i < 99; i++)
+		{
+			printf("%d", i);
+		}
+	}
+	else
+	{
+		for (i = n; i > 97; i--)
+		{
+			printf("%d", i);
+		}
 	}
 
 	printf("\n");
