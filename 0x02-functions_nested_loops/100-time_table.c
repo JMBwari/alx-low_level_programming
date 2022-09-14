f6daa011091035a667065e2ed2cb15eea392e843
JMBwari <ondieki.marion@gmail.com>
2022-09-14 07:56:35 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-time_table.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints  the n times table starting with 0
+ * @n: the n times table to print
+ * Return: void
+*/
+void print_times_table(void)
+{
+	if ((n > 15) || (n < 0))
+	{
+		/** do nothing */
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i * j < 10)
+				{
+					_putchar((i * j) + '0');
+				}
+				else if (i * j < 100)
+				{
+					_putchar(((i * j) / 10) + '0');
+					_putchar(((i * j) % 10) + '0');
+				}
+				else
+				{
+					_putchar(((i * j) / 100) + '0');
+					_putchar(((i * j) % 100) + '0');
+				}
+				/** not at last column, comma and space(s) */
+				if (j != n)
+				{
+					_putchar(',');
+					if (i * (j + 1) < 100)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else if (i * (j + 1) < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar(' ');
+					}
+				}
+				else
+				/** new line after last column */
+				{
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
