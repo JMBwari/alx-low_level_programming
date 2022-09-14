4aa462ab24065924d128a68e129c4e8b7d6051e4
JMBwari <ondieki.marion@gmail.com>
2022-09-14 09:46:43 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - entry point
+* Return: 0 if succesful
+*/
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		/** check if i is a multiple of 3 or 5 */
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			sum += i;
+		}
+	}
+
+	return (0);
+}
