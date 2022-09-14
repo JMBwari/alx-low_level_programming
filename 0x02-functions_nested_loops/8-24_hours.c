fc87c70ba8c3bd015ffd3a14ddb28f9f338e657d
JMBwari <ondieki.marion@gmail.com>
2022-09-13 23:56:55 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,5 +2,22 @@
 
 /**
  * jack_bauer: prints every minute of the day.
- * Return: 0 if successful
+ * Return: void
  */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 59; minute++)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(58);
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
