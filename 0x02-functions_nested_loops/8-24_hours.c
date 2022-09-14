818623e7a22fbdc7794d7540b1f205e2d29308ef
JMBwari <ondieki.marion@gmail.com>
2022-09-14 00:15:21 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,31 @@
  */
 void jack_bauer(void)
 {
+	/** the loop variables for the hour loop and the minute loop */
 	int hour, minute;
 
+	/** the first loop (going from hour 0 to hour 23 */
 	for (hour = 0; hour < 24; hour++)
 	{
+		/** the second loop (going from minute 0 to minute 59 */
 		for (minute = 0; minute < 60; minute++)
 		{
+			/** printing the leftmost digit of the hour */
 			_putchar(hour / 10 + '0');
+
+			/** printing the rightmost digit of the hour */
 			_putchar(hour % 10 + '0');
+
+			/** printing a colon */
 			_putchar(58);
+
+			/**printing the leftmost digit of the minute */
 			_putchar(minute / 10 + '0');
+
+			/**printing the rightmost digit of the minute */
 			_putchar(minute % 10 + '0');
+
+			/**printing the new line */
 			_putchar('\n');
 		}
 	}
