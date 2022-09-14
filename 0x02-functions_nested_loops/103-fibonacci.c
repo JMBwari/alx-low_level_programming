80a9e728a8bff75aeae9814b6b55249eb3949ac6
JMBwari <ondieki.marion@gmail.com>
2022-09-14 11:07:15 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,30 +7,28 @@
 
 int main(void)
 {
-  int i;
-  
-  long first, second, next, sum;
-  
-  first = 1;
-  second = 2;
-  next = first + second;
+	int i;
+	long first, second, next, sum;
 
-  sum = 2;
-  
-  for (i = 3; i < 34; i++)
-    {
-      first = second;
-      second = next;
-      next = first + second;
-      
-     if ((next % 2) == 0)
-     {
-	     sum += next;
-     }
-    }
-
-  printf("%li", sum);
-  printf("\n");
-
-  return (0);
+	first = 1;
+	second = 2;
+	next = first + second;
+	sum = 2;
+	
+	for (i = 3; i < 34; i++)
+	{
+		first = second;
+		second = next;
+		next = first + second;
+		
+		if ((next % 2) == 0)
+		{
+			sum += next;
+		}
+	}
+	
+	printf("%li", sum);
+	printf("\n");
+	
+	return (0);
 }
