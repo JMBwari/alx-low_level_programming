7a05f298d986ea51d3cc7aa3a82dbbc29765af1d
JMBwari <ondieki.marion@gmail.com>
2022-09-14 11:02:45 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+  int i;
+  
+  long first, second, next, sum;
+  
+  first = 1;
+  second = 2;
+  next = first + second;
+
+  sum = 2;
+  
+  for (i = 3; i < 34; i++)
+    {
+      first = second;
+      second = next;
+      next = first + second;
+      
+     if ((next % 2) == 0)
+     {
+	     sum += next;
+     }
+    }
+
+  printf("%li", sum);
+  printf("\n");
+
+  return (0);
+}
