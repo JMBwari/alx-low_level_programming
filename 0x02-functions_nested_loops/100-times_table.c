a7a8d9570bd665991da803d77bad4e5a190895a6
JMBwari <ondieki.marion@gmail.com>
2022-09-14 09:03:50 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 				{
 					_putchar(((i * j) / 100) + '0');
 					_putchar((((i * j) - 100) / 10) + '0');
-					_putchar(((i * j) % 100) + '0');
+					_putchar(((i * j) % 10) + '0');
 				}
 				/** not at last column, comma and space(s) */
 				if (j != n)
