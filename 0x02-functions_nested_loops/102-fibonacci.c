29ab6d78aac790849d51c15dac2c11ccccfab12a
JMBwari <ondieki.marion@gmail.com>
2022-09-14 10:07:02 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: void
+*/
+void main(void)
+{
+	int i;
+
+	for (i = 1; i < 51; i++)
+	{
+		if (i == 1)
+		{
+			printf("%d", i);
+			printf(", ");
+		}
+
+		if (i == 2)
+		{
+			printf("%d", i);
+			printf(", ");
+		}
+
+		printf("%d", (i - 2) + (i - 1));
+		
+		if (i < 50)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}
