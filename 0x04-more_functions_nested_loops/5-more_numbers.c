43314be629f91e0d5d292caedb38bd7a2c5e16d2
JMBwari <ondieki.marion@gmail.com>
2022-09-22 11:41:43 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,22 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			k = (j >= 10) ? ((j / 10) + 48) : (j + 48);
+
+			_putchar(k);
+
 			if (j >= 10)
 			{
-				_putchar((j / 10) + 48);
 				_putchar((j % 10) + 48);
 			}
 		}
+
 		_putchar('\n');
 	}
 }
