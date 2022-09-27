0e0531155789b731d3bf3535cd5e1ef3773fc66f
JMBwari <ondieki.marion@gmail.com>
2022-09-26 23:07:22 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int and updates the value
  * @n: pointer to n
- * Return: 0 if successfu
+ * Return: 0 if successful
  */
 void reset_to_98(int *n)
 {
