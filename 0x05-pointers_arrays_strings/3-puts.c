b639907b10ec8bc7113c36b59354eaa676f6ddcb
JMBwari <ondieki.marion@gmail.com>
2022-09-27 01:52:10 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: variable string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i; /* the loop variable */
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar("\n");
+}
