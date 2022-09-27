dd367aab0f817b15776d8d2b644c73f693d6afec
JMBwari <ondieki.marion@gmail.com>
2022-09-26 22:40:10 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int and updates the value
+ * Return: 0 if successfu
+ */
+void reset_to_98(int *n)
+{
+	&n = 98;
+
+	return (0);
+}
