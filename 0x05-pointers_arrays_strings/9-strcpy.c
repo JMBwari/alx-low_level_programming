65a0d153547c266388a4687ef32fd56e739b8eba
JMBwari <ondieki.marion@gmail.com>
2022-10-02 04:40:01 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	size_t i;
-	size_t n = strlen(src);
+	size_t n = 0;
+
+	for (i = 1; i < n && src[i] != '\0'; i++)
+		n = i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
