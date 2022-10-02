157c85ca7d927cc25d8e21dc1941dc5ef44115d0
JMBwari <ondieki.marion@gmail.com>
2022-10-02 04:02:05 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8_print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_array - prints the elements of the array a
+* @a: array variable
+* @n: length of the array
+* Return: void
+*/
+void print_array(int *a, int n)
+{
+	int i = 0; /* loop variable */
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+
+	printf("%d", a[n - 1]);
+
+	printf("\n");
+}
