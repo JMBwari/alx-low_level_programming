c2151e332eaff339fbeaa1fa4aa3449cc2d74c8a
JMBwari <ondieki.marion@gmail.com>
2022-10-03 07:50:01 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints an integer
+ *
+ * @n: number to be printed
+ * Return: void
+ *
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		n *= -1;
+		i = n;
+		_putchar('45');
+	}
+}
